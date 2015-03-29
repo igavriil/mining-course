b08f9286d80b48407d450d27f0d54ae8d9526003
igavriil <iasongavriil@gmail.com>
2015-03-29 14:07:47 +0300

    fix similarity testing with prefix

--- set7/solveIndexingSimilarity.py
+++ set7/solveIndexingSimilarity.py
@@ -1,9 +1,27 @@
 from collections import defaultdict
 
+
 def calclulate_prefix(jaccard_sim, string):
-    p = int((1-jaccard_sim)*len(string)) + 1
+    p = int(jaccard_sim*len(string) + 1)
     return p
 
+
+def create_index_buckets(string_set, jaccard_sim):
+    index_buckets = defaultdict(list)
+    for string in string_set:
+        for symbol in string[:calclulate_prefix(jaccard_sim, string)]:
+            index_buckets[symbol].append(string)
+
+    return index_buckets
+
+
+def to_compare(index_buckets, probe_string, jaccard_sim):
+    compare_set = set()
+    for symbol in probe_string[:calclulate_prefix(jaccard_sim, probe_string)]:
+        compare_set.update(index_buckets[symbol])
+        compare_set.discard(probe_string)
+
+    return compare_set
 
 s1 = "abcef"
 s2 = "acdeg"
@@ -14,19 +32,12 @@ s6 = "bceg"
 s7 = "cdfg"
 s8 = "abcd"
 
-string_set = [s2, s3, s4, s5, s6, s7, s8]
+string_set = [s1, s2, s3, s4, s5, s6, s7, s8]
 
+index_buckets = create_index_buckets(string_set, 0.2)
 
-print calclulate_prefix(0.2, s1)
-
-to_search = set()
-indexes = defaultdict(list)
-for symbol in s1[:calclulate_prefix(0.2, s1)]:
-    indexes[symbol].append(s1)
-    for string in string_set:
-        if symbol in string:
-            indexes[symbol].append(string)
-            to_search.update([string])
-
-print indexes
-print to_search
+for probe_string in [s1, s3, s6]:
+    print probe_string
+    compare_set = to_compare(index_buckets, probe_string, 0.2)
+    print compare_set
+    print len(compare_set)
